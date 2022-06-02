f673403561b9cd9da96a485d097556541ce5ab3b
root <root@ncpp.ddns.net>
2022-06-02 18:21:14 +0800

    almost work

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker rm -vf $(docker ps -aq)
+docker network prune
+docker volume rm $(docker volume ls -q)
--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -9,7 +9,7 @@
 ##    docker volume rm <name>_jupyterhub_data
 ##
 
-import os
+import os, sys
 
 ## Generic
 c.JupyterHub.admin_access = True
@@ -33,17 +33,25 @@ c.JupyterHub.load_roles = [
 ## Authenticator
 # https://blog.jupyter.org/simpler-authentication-for-small-scale-jupyterhubs-with-nativeauthenticator-999534c77a09
 c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
+c.NativeAuthenticator.open_signup = True
+c.Authenticator.admin_users = { 'shoh' ,'root' }
+
+c.NativeAuthenticator.check_common_password = True
+
+c.NativeAuthenticator.allowed_failed_logins = 3
+c.NativeAuthenticator.seconds_before_next_try = 1200
 
 ## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.DockerSpawner.debug = True
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') #or '/home/jovyan'
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
