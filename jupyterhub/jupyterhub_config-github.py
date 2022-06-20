195612f1046664100a0e29491447a9bbe8afdc0f
SiewYan <siew.yan.hoh@cern.ch>
2022-06-20 15:44:49 +0800

    switch to native github login

--- jupyterhub/jupyterhub_config-github.py
+++ jupyterhub/jupyterhub_config-github.py
@@ -40,63 +40,18 @@ c.JupyterHub.load_roles = [
 
 #c.NativeAuthenticator.allowed_failed_logins = 3
 #c.NativeAuthenticator.seconds_before_next_try = 1200
-#################################################################
 
-#from oauthenticator.github import GitHubOAuthenticator
-#c.JupyterHub.authenticator_class = GitHubOAuthenticator
+from oauthenticator.github import GitHubOAuthenticator
+c.JupyterHub.authenticator_class = GitHubOAuthenticator
 
-#c.GitHubOAuthenticator.create_system_users = True
-
-#c.Authenticator.allowed_users = allowed_users = set()
-#c.JupyterHub.admin_users = admin = set()
-
-#c.MyOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-#c.MyOAuthenticator.client_id = os.environ['OAUTH_CLIENT_ID']
-#c.MyOAuthenticator.client_secret = os.environ['OAUTH_CLIENT_SECRET']
-
-#################################################################
-
-c = get_config()
-
-c.JupyterHub.log_level = 10
-from oauthenticator.github import LocalGitHubOAuthenticator
-
-c.JupyterHub.authenticator_class = LocalGitHubOAuthenticator
-
-c.LocalGitHubOAuthenticator.create_system_users = True
+c.GitHubOAuthenticator.create_system_users = True
 
 c.Authenticator.allowed_users = allowed_users = set()
 c.JupyterHub.admin_users = admin = set()
 
-join = os.path.join
-
-here = os.path.dirname(__file__)
-root = os.environ.get('OAUTHENTICATOR_DIR', here)
-sys.path.insert(0, root)
-
-with open(join(root, 'userlist')) as f:
-    for line in f:
-        if not line:
-            continue
-        parts = line.split()
-        name = parts[0]
-        allowed_users.add(name)
-        if len(parts) > 1 and parts[1] == 'admin':
-            admin.add(name)
-
-c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-
-# ssl config
-ssl = join(root, 'ssl')
-keyfile = join(ssl, 'ssl.key')
-certfile = join(ssl, 'ssl.cert')
-if os.path.exists(keyfile):
-    c.JupyterHub.ssl_key = keyfile
-if os.path.exists(certfile):
-    c.JupyterHub.ssl_cert = certfile
-
-print("c.Authenticator.allowed_users : ", c.Authenticator.allowed_users)
-print("c.JupyterHub.admin_users : ", c.JupyterHub.admin_users)
+c.MyOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+c.MyOAuthenticator.client_id = os.environ['OAUTH_CLIENT_ID']
+c.MyOAuthenticator.client_secret = os.environ['OAUTH_CLIENT_SECRET']
 
 #################################################################
 
